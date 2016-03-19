c4377ebf9501121371a8569f871d0c306caac121
mitov22 <ursamidorfeed@outlook.com>
2016-03-19 22:53:10 +0600

    create blog create about

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='',
+    version='',
+    packages=['blog', 'blog.migrations', 'MyBlog'],
+    url='',
+    license='',
+    author='User',
+    author_email='',
+    description=''
+)
